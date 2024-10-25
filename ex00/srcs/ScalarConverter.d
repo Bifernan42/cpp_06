f14613a34d6fe01a9bad75ac9f7d3739d568024f
Superinspired <bifernan@student.42.fr>
2024-10-25 13:34:12 +0200

    second commit

--- ex00/srcs/ScalarConverter.d
+++
@@ -1,4 +0,0 @@
-srcs/ScalarConverter.o: srcs/ScalarConverter.cpp \
-  srcs/../includes/ScalarConverter.hpp
-
-srcs/../includes/ScalarConverter.hpp:
--- ex00/srcs/main.d
+++
@@ -1,3 +0,0 @@
-srcs/main.o: srcs/main.cpp srcs/../includes/ScalarConverter.hpp
-
-srcs/../includes/ScalarConverter.hpp:
